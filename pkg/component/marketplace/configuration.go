dd8d669e5726686d7875928e4072fd8360a7a846
Renato Guimaraes <renatoaguimaraes@gmail.com>
2021-11-18 10:53:35 -0300

    fix(marketplace): export configuration variables (#30)

--- pkg/component/marketplace/configuration.go
+++ pkg/component/marketplace/configuration.go
@@ -4,8 +4,8 @@ import "github.com/kelseyhightower/envconfig"
 
 // AzureMarketplaceConfiguration represents the configuration for marketplace client.
 type AzureMarketplaceConfiguration struct {
-	resourceUri string `envconfig:"AZURE_MANAGED_APP_RESOURCE_URI" required:"true"`
-	planId      string `envconfig:"AZURE_MANAGED_APP_PLAN_ID" required:"true"`
+	ResourceUri string `envconfig:"AZURE_MANAGED_APP_RESOURCE_URI" required:"true"`
+	PlanId      string `envconfig:"AZURE_MANAGED_APP_PLAN_ID" required:"true"`
 }
 
 // LoadFromEnvVars reads all env vars required for the marketplace client.
