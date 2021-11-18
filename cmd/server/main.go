392878da39b55229987dfaf235cb3b57eee917b1
Wallace Salles <wallace_robinson@hotmail.com>
2021-11-18 12:32:08 -0300

    feat: settings, default logging and go-core support (#29)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,37 +5,41 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ydataai/azure-adapter/pkg/common"
+	"github.com/ydataai/go-core/pkg/common/config"
+	"github.com/ydataai/go-core/pkg/common/logging"
+	"github.com/ydataai/go-core/pkg/common/server"
+
 	"github.com/ydataai/azure-adapter/pkg/component/usage"
 	"github.com/ydataai/azure-adapter/pkg/controller"
-	"github.com/ydataai/azure-adapter/pkg/server"
 	"github.com/ydataai/azure-adapter/pkg/service"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-12-01/compute"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
+)
 
-	"github.com/sirupsen/logrus"
+var (
+	errChan chan error
 )
 
 func main() {
-	applicationConfiguration := configuration{}
+	applicationConfiguration := Configuration{}
 	restServiceConfiguration := service.RESTServiceConfiguration{}
-	serverConfiguration := server.Configuration{}
-	restControllerConfiguration := controller.RESTControllerConfiguration{}
+	serverConfiguration := server.HTTPServerConfiguration{}
+	restControllerConfiguration := config.RESTControllerConfiguration{}
+	loggerConfiguration := logging.LoggerConfiguration{}
 
-	err := initConfigurationVariables([]common.ConfigurationVariables{
+	if err := config.InitConfigurationVariables([]config.ConfigurationVariables{
 		&applicationConfiguration,
 		&restServiceConfiguration,
 		&serverConfiguration,
 		&restControllerConfiguration,
-	})
-	if err != nil {
+		&loggerConfiguration,
+	}); err != nil {
 		fmt.Println(fmt.Errorf("could not set configuration variables. Err: %v", err))
 		os.Exit(1)
 	}
 
-	var logger = logrus.New()
-	logger.SetLevel(applicationConfiguration.logLevel)
+	logger := logging.NewLogger(loggerConfiguration)
 
 	authorizer, err := auth.NewAuthorizerFromEnvironment()
 	if err != nil {
@@ -43,7 +47,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	computeUsageClient := compute.NewUsageClient(applicationConfiguration.subscriptionID)
+	computeUsageClient := compute.NewUsageClient(applicationConfiguration.SubscriptionID)
 	computeUsageClient.Authorizer = authorizer
 
 	usageClient := usage.NewUsageClient(logger, computeUsageClient)
@@ -52,20 +56,11 @@ func main() {
 	restController := controller.NewRESTController(logger, restService, restControllerConfiguration)
 
 	serverCtx := context.Background()
-	s := server.NewServer(logger, serverConfiguration)
-	restController.Boot(s)
-	s.Run(serverCtx)
+	httpServer := server.NewServer(logger, serverConfiguration)
+	restController.Boot(httpServer)
+	httpServer.Run(serverCtx)
 
-	for err := range s.ErrCh {
+	for err := range errChan {
 		logger.Error(err)
 	}
-}
-
-func initConfigurationVariables(configurations []common.ConfigurationVariables) error {
-	for _, configuration := range configurations {
-		if err := configuration.LoadEnvVars(); err != nil {
-			return err
-		}
-	}
-	return nil
 }
--- go.mod
+++ go.mod
@@ -4,28 +4,29 @@ go 1.17
 
 require (
 	github.com/Azure/azure-sdk-for-go v52.5.0+incompatible
-	github.com/Azure/go-autorest/autorest v0.11.17
+	github.com/Azure/go-autorest/autorest v0.11.18
 	github.com/Azure/go-autorest/autorest/azure/auth v0.5.7
 	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
 	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.4
 	github.com/golang/mock v1.5.0
 	github.com/google/go-cmp v0.5.5
-	github.com/google/uuid v1.2.0
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/sirupsen/logrus v1.8.1
-	github.com/ydataai/go-core v0.0.0-20211117134902-51b794d29554
+	github.com/sirupsen/logrus v1.8.1 // indirect
 )
+
+require github.com/ydataai/go-core v0.1.0
 
 require (
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.9.11 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.13 // indirect
 	github.com/Azure/go-autorest/autorest/azure/cli v0.4.2 // indirect
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
-	github.com/Azure/go-autorest/logger v0.2.0 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/dimchansky/utfbom v1.1.1 // indirect
-	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
+	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -41,6 +42,7 @@ require (
 	golang.org/x/crypto v0.0.0-20211115234514-b4de73f9ece8 // indirect
 	golang.org/x/sys v0.0.0-20211116061358-0a5406a5449c // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
